678c2f9839df70054daaeeb3ecb72fb45d07e9c4
Cpftfu <alexseev45s@gmail.com>
2023-09-25 13:09:25 +0300

    Добавила работу

--- cumculatorPython.py
+++ cumculatorPython.py
@@ -61,78 +61,126 @@ while (d):
     print("9. Косинус от числа")
     print("10. Тангенс от числа")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    
+   
+
     while True:
-      try:
-        menu = int(input("Выберите действие которое выхотите выполнить: "))
-        break
-      except ValueError:
-        print("Упси")
+        try:
+           menu = int(input("Выберите действие которое выхотите выполнить: "))
+           break
+        except ValueError:
+             print("Упси")
 
     if int(menu) == 1:
-        result1 = slogenie(a1=int(input("Введите первое число: ")),
+        while True:
+             try:
+                  result1 = slogenie(a1=int(input("Введите первое число: ")),
                        b1=int(input("Введите второе число:  ")))
-        print("сумма чисел равняется :", result1)
+                  print("сумма чисел равняется :", result1)
+                  break
+             except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 2:
-        result2 = vichitanie(a2=int(input("Введите первое число: ")),
+        while True:
+             try: 
+                  result2 = vichitanie(a2=int(input("Введите первое число: ")),
                               b2=int(input("Введите второе число: ")))
-        print("разность чисел равняется :", result2)
+                  print("разность чисел равняется :", result2)
+                  break
+             except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 3:
-        result3 = umnogenie(a3=int(input("Введите первое число: ")),
+        while True:
+             try: 
+                  result3 = umnogenie(a3=int(input("Введите первое число: ")),
                                  b3=int(input("Введите второе число: ")))
-        print("произведение чисел равняется :", result3)
+                  print("произведение чисел равняется :", result3)
+                  break
+             except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 4:
-        result4 = delenie(a4=int(input("Введите первое число: ")),
+        while True:
+             try:
+                  result4 = delenie(a4=int(input("Введите первое число: ")),
                            b4=int(input("Введите второе число: ")))
-        print("деление чисел равняется :", result4)
+                  print("деление чисел равняется :", result4)
+                  break
+             except ValueError:
+                  print("Упси")
        
 
         
     elif int(menu) == 5:
-        result5 = stepen(a5=int(input("Введите число: ")),
+         while True:
+              try:
+                   result5 = stepen(a5=int(input("Введите число: ")),
                           b5=int(input("Введие степень в которую хотите возвести число: ")))
-        print("число в степени равняется :", result5)
+                   print("число в степени равняется :", result5)
+                   break
+              except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 6:
-        result6 = kvadratkoren(a6=int(input("Введите число: ")))
-        print("корень равняется :", result6)  
+          while True:
+               try:
+                    result6 = kvadratkoren(a6=int(input("Введите число: ")))
+                    print("корень равняется :", result6)
+                    break
+               except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 7:
-        result7 = factorial(a7=int(input("Введите число: ")))
-        print("факториал из числа равняется :", result7) 
+         while True:
+               try:
+                    result7 = factorial(a7=int(input("Введите число: ")))
+                    print("факториал из числа равняется :", result7) 
+                    break
+               except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 8:
-        result8 = sinus(a8=int(input("Введите число: ")))
-        print("синус числа равняется :", result8) 
+        while True:
+               try:
+                    result8 = sinus(a8=int(input("Введите число: ")))
+                    print("синус числа равняется :", result8) 
+                    break
+               except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 9:
-        result9 = cosinus(a9=int(input("Введите число: ")))
-        print("косинус числа равняется :", result9)
+        while True:
+               try:
+                    result9 = cosinus(a9=int(input("Введите число: ")))
+                    print("косинус числа равняется :", result9)
+                    break
+               except ValueError:
+                  print("Упси")
 
 
     elif int(menu) == 10:
-        result10 = tangens(a10=int(input("Введите число: ")))
-        print("тангенс числа равняется :", result10) 
+        while True:
+               try:
+                    result10 = tangens(a10=int(input("Введите число: ")))
+                    print("тангенс числа равняется :", result10) 
+                    break
+               except ValueError:
+                  print("Упси")
 
     
     elif int(menu) < 1 or int(menu) > 10:
         print("Так нельзя, вы чево")
+    
 
+    go_again = input("Хотите продолжить выполнять действия? (да или нет)")
+    if go_again == "нет":
+        break
 
-    go_again = input("Хотите выполнить еще действие? (да/нет): ")
-    if (go_again.lower()=="нет"):
-        d = False
-        print("пака-пака")
-    else:
-        continue
-      
